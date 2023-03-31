8e66a3c0975ea33ba403d385db0d784e3b61260a
Lilly Daniell <daniell.lilly@gmail.com>
2023-03-31 14:39:02 +0100

    Landing page and report page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import ReportPage from "./components/reportPage";
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+// import ReactDOM from "react-dom/client";
+import LandingPage from "./pages/LandingPage"
+import ReportPage from "../src/pages/ReportPage"
+
 
 function App() {
-  return (
-    <div className="App">
-      <ReportPage />
-     
-        
-    </div>
+  return (<>  
+  <Routes>
+    <Route path="/" element={<LandingPage/>} />
+    <Route path="/report" element={<ReportPage/>} />
+  </Routes></>
+       
   );
 }
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
 
 export default App;
